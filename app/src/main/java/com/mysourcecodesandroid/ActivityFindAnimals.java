a5b27053bf5b0c8b9a8f0fefd307d15840ba6ef5
gurkay <g_basyigit@hotmail.com>
2020-03-19 07:40:47 +0300

    New Examples find animals files update

--- app/src/main/java/com/mysourcecodesandroid/ActivityFindAnimals.java
+++ app/src/main/java/com/mysourcecodesandroid/ActivityFindAnimals.java
@@ -8,6 +8,39 @@ import android.view.MenuItem;
 
 public class ActivityFindAnimals extends AppCompatActivity {
 
+    String questionFindAnimals[] = {
+            "Find the bald eagle, bald eagle makes sound...!",
+            "Find the bear, bear makes sound...!",
+            "Find the beluga whale, beluga whale makes sound...!",
+            "Find the cat, cat makes sound...!",
+            "Find the jaguar, jaguar makes sound...!",
+            "Find the killer whale, killer whale makes sound...!",
+            "Find the lion, lion makes sound...!",
+            "Find the penguin, penguin makes sound...!",
+            "Find the sea lion, sea lion makes sound...!",
+            "Find the pig, pig makes sound...!",
+            "Find the gray wolf, gray wolf makes sound...!",
+            "Find the leopard, leopard makes sound...!",
+            "Find the frog, frog makes sound...!",
+            "Find the hippo, hippo makes sound...!",
+            "Find the chimps, chimps makes sound...!",
+            "Find the elephant, elephant makes sound...!",
+            "Find the bengal tiger, bengal tiger makes sound...!",
+            "Find the dolphin, dolphin makes sound...!",
+            "Find the dog, dog makes sound...!"
+    };
+
+    int picturesFindAnimals[] = {
+            R.drawable.bald_eagle, R.drawable.bear, R.drawable.beluga_whale,
+            R.drawable.cat, R.drawable.jaguar, R.drawable.killer_whale,
+            R.drawable.lion, R.drawable.penguin, R.drawable.sea_lion,
+            R.drawable.pig, R.drawable.gray_wolf, R.drawable.leopard,
+            R.drawable.frog, R.drawable.hippo, R.drawable.chimps, R.drawable.elephant,
+            R.drawable.bengal_tiger, R.drawable.dolphin, R.drawable.dog
+    };
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
