102bd17e53ae766e49c7cea29c5bd040a94926c5
Gürkay Başyiğit <g_basyigit@hotmail.com>
2020-03-20 14:45:09 +0300

    Update ActivitySoundsAndMusic.java

--- app/src/main/java/com/mysourcecodesandroid/ActivitySoundsAndMusic.java
+++ app/src/main/java/com/mysourcecodesandroid/ActivitySoundsAndMusic.java
@@ -1,1 +1,62 @@
+package com.mysourcecodesandroid;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class ActivitySoundsAndMusic extends AppCompatActivity {
+    
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+    
+    private CountDownTimer mCountDownTimer;
+    
+    private boolean mTimerRunning;
+    
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    
+    Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countdown_timer);
+        init();
+    }
+
+    public void init() {
+        // toolbar add
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+        
+        mButtonStartPause = findViewById(R.id.button_start_pause);
+        mButtonReset = findViewById(R.id.button_reset);
+            
+        mButtonStartPause.setOnClickListener(new View.onClickListener() {
+          @Override
+          public void onClick(View v) {
+
+          }
+        });
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        int itemId = menuItem.getItemId();
+
+        if(itemId == R.id.home) {
+            this.finish();
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+}
