e89079392782ed08f30b0acbc7e1d96fc0d67afa
gurkay <g_basyigit@hotmail.com>
2020-04-29 03:05:13 +0300

    New Examples find animals files update

--- app/src/main/java/com/mysourcecodesandroid/ActivityJSoupLibrary.java
+++ app/src/main/java/com/mysourcecodesandroid/ActivityJSoupLibrary.java
@@ -1,14 +1,21 @@
 package com.mysourcecodesandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -19,9 +26,11 @@ import java.io.IOException;
 
 public class ActivityJSoupLibrary extends AppCompatActivity implements View.OnClickListener{
 
-    TextView j_soup_txtresult;
+    TextView j_soup_txtlink;
     Button j_soup_btntab1,j_soup_btntab2,j_soup_btntab3;
     Context context = this;
+    LinearLayout j_soup_pnl_add;
+
     private String webSiteUrl;
     public String findContent;
 
@@ -45,7 +54,9 @@ public class ActivityJSoupLibrary extends AppCompatActivity implements View.OnCl
         j_soup_btntab2.setOnClickListener(this);
         j_soup_btntab3.setOnClickListener(this);
 
-        j_soup_txtresult = findViewById(R.id.j_soup_txtresult);
+        j_soup_txtlink = findViewById(R.id.j_soup_txtlink);
+
+        j_soup_pnl_add = findViewById(R.id.j_soup_pnl_add);
 
     }
 
@@ -69,22 +80,18 @@ public class ActivityJSoupLibrary extends AppCompatActivity implements View.OnCl
 
                         Elements links = doc.select("div.caContent a[href]");
                         log("Here : ", "AYBU Duyurular");
+
                         for(Element link : links) {
-                            log(link.text() + " Link : ", link.attr("href"));
-
-                            builder.append("\n").append("Link : ").append(link.attr("href"))
-                                    .append("\n").append("Text : ").append(link.text());
+                            builder.append("\n").append(link.attr("href"))
+                                    .append("\n").append(link.text());
                         }
-
 
                     } else if (findContent.equalsIgnoreCase("cnContent")) {
                         Elements links = doc.select("div.cnContent a[href]");
                         log("Here : ", "AYBU Haberler");
                         for(Element link : links) {
-                            log(link.text() + " Link : ", link.attr("href"));
-
-                            builder.append("\n").append("Link : ").append(link.attr("href"))
-                                    .append("\n").append("Text : ").append(link.text());
+                            builder.append("\n").append(link.attr("href"))
+                                    .append("\n").append(link.text());
                         }
                     } else {
 
@@ -92,10 +99,9 @@ public class ActivityJSoupLibrary extends AppCompatActivity implements View.OnCl
                         log("Here : ", "Yemek Menu");
                         for(Element link : links) {
                             if(findContent.equalsIgnoreCase(link.text())) {
-                                builder.append("\n").append("Link : ").append(link.attr("href"))
-                                        .append("\n").append("Text : ").append(link.text());
+                                builder.append("\n").append(link.attr("href"))
+                                        .append("\n").append(link.text());
                             }
-
                         }
                     }
                 }catch (IOException e) {
@@ -105,7 +111,34 @@ public class ActivityJSoupLibrary extends AppCompatActivity implements View.OnCl
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        j_soup_txtresult.setText(builder.toString());
+
+                        String[] text = builder.toString().split("\n");
+                        j_soup_txtlink.setText(text[0]);
+                        j_soup_pnl_add.removeAllViews();
+                        String textLink = "";
+                        for (int i=2; i < text.length; i++) {
+                            if(i%2 == 0) {
+                                textLink = text[i].toString();
+                            } else {
+                                TextView textView = new TextView(context);
+                                log("Text : ", text[i].toString());
+                                SpannableString ss = new SpannableString(text[i]);
+
+                                final String finalTextLimk = textLink;
+                                ClickableSpan clickableSpan = new ClickableSpan() {
+                                    @Override
+                                    public void onClick(View widget) {
+                                        Toast.makeText(context, finalTextLimk , Toast.LENGTH_LONG).show();
+                                    }
+                                };
+
+                                ss.setSpan(clickableSpan, 0, text[i].length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                                textView.setText(ss);
+                                textView.setMovementMethod(LinkMovementMethod.getInstance());
+                                j_soup_pnl_add.addView(textView);
+                            }
+                        }
                     }
                 });
             }
